4937baebc26c5de8312d512a91d89dc74787a2c4
andrew <neon.duncan@gmail.com>
2021-06-07 09:45:26 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting
+  if (feet <= 400) {
+    greeting = "This one is on me!"
+  } else if (feet > 2000 && feet < 2500){
+    greeting = 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    greeting = "No can do."
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const message = city === "NYC" ? "Ok, sounds good." : "No go."
+  return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let message;
+  switch (tip) {
+    case "generous":
+      message = "Thank you so much."
+      break;
+    case "not as generous":
+      message = "Thank you."
+      break;
+    default:
+      message = "Bye."
+  }
+  return message
 }
